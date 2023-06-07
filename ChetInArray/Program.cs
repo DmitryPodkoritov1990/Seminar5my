9d5c454ad171786c4e90d6fe5f93b856629cbb47
Dmitry Podkoritov <mitrofun1990@gmail.com>
2023-06-08 00:38:13 +0500

    add Find

---
+++ ChetInArray/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив, заполненный случайными положительными трехзначными числами. 
+//Напишите программу, которая покажет количество четных чисел в массиве.
+
+int[] myArray90(int size)
+{
+    int[] triple = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        triple[i] = new Random().Next(100, 1000);
+        
+    }
+    return triple;
+}
+
+void PrintnewArray(int[] arrow)
+{
+    for (int i = 0; i < arrow.Length; i++)
+    {
+        Console.Write($"{arrow[i]} ");
+    }
+}
+
+void FindChetSum(int[] arr1)
+{
+    
+    int count = 0;
+    foreach (int el in arr1)
+    {
+    if (el % 2 == 0)
+    {
+        count += 1; // positiveSum = positiveSum +  el
+    }  
+    }
+    Console.WriteLine(count);
+}
+
+int[] mynewArr = myArray90(5);
+PrintnewArray(mynewArr);
+Console.WriteLine(" ");
+FindChetSum(mynewArr);
+
---
+++ FindMaxMinDouble/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+
+   
+Console.Clear ();
+
+double[] arrayRan = new double[10];
+Random rand = new Random();
+ for (int i = 0; i < arrayRan.Length; i++)
+ {
+    arrayRan[i] = rand.NextDouble()*100;
+    Console.WriteLine(arrayRan[i]);
+ }
+
+
+    double max = arrayRan[0];  
+
+    for (int i = 0; i < arrayRan.Length; i++)
+    {
+ 
+    if (arrayRan[i] > max)
+    {
+        
+        arrayRan[i] = max;
+    }  
+
+     Console.WriteLine("Max " + arrayRan[i]);
+}
+
+
+
